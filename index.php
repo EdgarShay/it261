3c3902c2b786a7ba03bdb2696b76cdb1af872d42
EdgarShay <97201888+EdgarShay@users.noreply.github.com>
2023-01-16 20:12:14 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portal Page</title>
+    <link href="css/styles.css" type="text/css"
+    rel="stylesheet">
+</head>
+<body>
+<h1><a href="index.php">Edgar's Portal Page</a></h1>
+<h2>The navigation below represents our Big 
+Assignments. </h2>
+
+
+ <div id="wrapper">
+<nav>
+<ul>
+<li><a href="">Switch</a></li>
+<li><a href="">Troubleshoot</a></li>
+<li><a href="">Claculator</a></li>
+<li><a href="">Email</a></li>
+<li><a href="">Database</a></li>
+<li><a href="">Gallery</a></li>
+</ul>
+</nav>
+<div id="row">
+
+
+</div>
+
+<main>
+
+<h2>About Edgar</h2>
+<img class="right" src="images/Edgar.jpg" alt="My-image">
+
+<p>Hello my name is Edgar Vargas. I moved to Seattle from Puerto Vallarta, Mexico 
+almost eight years ago. My first language is Spanish. This is my fifth Web Develepment class
+here at Seattle Central College. I have a wobnderful wife and a handsome boy.
+My goal is to earn my certificate in Web Development and eventually get my degree
+in Computer Science. By gaining experience and skills, I plan to grow my career. 
+Besides my education I have two real passions. The first is spending time with my wife and son.
+The other passion is to working out in the gym.  </p>
+<img class="left" src="images/screenshot.jpg" alt="MAMP">
+<img class="left" src="images/screenshot.2.jpg" alt="MAMP">
+<img class="left" src="images/screenshot.3.jpg" alt="MAMP">
+
+</main>
+
+<aside>
+
+<h2>Weekly Class Exercises</h2>
+<h3>Week 2</h3>
+<ol>
+<li><a href="weeks/week2/var.php">var.php</a></li>
+<li><a href="weeks/week2/var2.php">var2.php</a></li>
+<li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
+<li><a href="weeks/week2/currency.php">currency.php</a></li>
+<li><a href="weeks/week2/heredoc.php.">heredoc.php</a></li>
+</ol>
+
+<h3>Week 3</h3>
+<ol>
+<li><a href="weeks/week2/if.php">if.php</a></li>
+<li><a href="weeks/week2/foreach.php">foreach.php</a></li>
+<li><a href="weeks/week2/forloop.php">forloop.php</a></li>
+<li><a href="weeks/week2/date.php">date.php</a></li>
+<li><a href="weeks/week2/switch.php">switch.php</a></li>
+</ol>
+
+<h3>Week 4</h3>
+<ol>
+<li><a href="weeks/week2/form1.php">form1.php</a></li>
+<li><a href="weeks/week2/form2.php">form2.php</a></li>
+<li><a href="weeks/week2/form3.php">form3.php</a></li>
+<li><a href="weeks/week2/form-arithmethic.php">form-aritmethic.php</a></li>
+<li><a href="weeks/week2/celsius.php">switch.php</a></li>
+</ol>
+
+<h3>Week 5</h3>
+<ol>
+<li><a href="weeks/week2/currency1.php">currency1.php</a></li>
+<li><a href="weeks/week2/currency2.php">currency2.php</a></li>
+<li><a href="weeks/week2/currency3.php">currency3.php</a></li>
+<li><a href="weeks/week2/currency4.php">currency4.php</a></li>
+<li><a href="weeks/week2/null.php">null.php</a></li>
+</ol>
+
+<h3>Week 6</h3>
+<ol>
+<li><a href="weeks/week2/form1.php">form1.php</a></li>
+<li><a href="weeks/week2/functions.php">functions.php</a></li>
+<li><a href="weeks/week2/implode.php">implode.php</a></li>
+</ol>
+
+<h3>Week 7</h3>
+<ol>
+<li><a href="weeks/week2/pictures.php">pictures.php</a></li>
+<li><a href="weeks/week2/random.php">ramdom.php</a></li>
+<li><a href="weeks/week2/strings.php">strings.php</a></li>
+</ol>
+
+<h3>Week 8</h3>
+<ol>
+<li><a href="weeks/week2/people.php">people.php</a></li>
+</ol>
+
+</aside>
+
+<footer>
+    <ul>
+        <li>Copyright &copy; 2023</li>
+        <li>All Rights Reserved</li>
+        <li><a href="">Terms of use</a></li>
+        <li><a href="../index.html">Web Design by Edgar</a></li>
+        <li><a href="">
+        <img src="images/html5.png" alt="HTML 5 logo">
+        </a></li>
+    </ul>
+    
+</footer>
+
+
+<!-- close wrapper -->
+</div>
+
+</body>
+</html>
