4803ec4b7608adfa771efff17c8c9b2085f345f3
EdgarShay <97201888+EdgarShay@users.noreply.github.com>
2023-01-16 23:14:22 -0800

    Add files via upload

--- gallery.php
+++ gallery.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Switch page of my Portal Page</title>
+    <title>Gallery page of my Portal Page</title>
     <link href="css/styles.css" type="text/css"
     rel="stylesheet">
 </head>
-<body class="Switch">
+<body class="Gallery">
 <h1><a href="index.php">Edgar's Portal Page</a></h1>
 <h2>The navigation below represents our Big 
 Assignments. </h2>
