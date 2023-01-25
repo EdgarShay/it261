b0e0ea2f0094fa765fe3544ea985effcc45b0414
EdgarShay <97201888+EdgarShay@users.noreply.github.com>
2023-01-25 14:02:30 -0800

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,49 @@
+<?php include('./includes/header.php'); ?>
+
+<div id="wrapper">
+
+<div id="hero">
+<img src="images/twelve.jpg" alt="Twelve is greater than 3">
+
+
+<!--end hero-->
+</div> 
+
+<main>
+<h1>Welcome to our Web App Programming Class!</h1>
+<h2>We are going to learn PHP!</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna
+aliqua. Ut enim ad minim veniam, quis nostrud 
+exercitationullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit 
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+<h2>Another Headline 2!</h2>
+<p>Sed ut perspiciatis unde omnis iste natus error 
+sit voluptatem accusantium doloremque laudantium, totam rem
+aperiam, eaque ipsa quae ab illo inventore veritatis 
+et quasi architecto beatae vitae dicta sunt explicabo. 
+Nemo enim ipsam voluptatem quia voluptas sit aspernatur 
+aut odit aut fugit, sed quia consequuntur magni dolores 
+eos qui ratione voluptatem sequi nesciunt. Neque porro 
+quisquam est, qui dolorem ipsum quia dolor sit amet, 
+consectetur,</p>
+</main>
+
+<aside>
+<h3>This is our headline three in our beautiful aside</h3>
+<p>Ut enim ad minim veniam, quis nostrud 
+exercitationullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit 
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+</aside>
+
+<!--end wrapper-->
+</div> 
+
+<?php
+include('./includes/footer.php');
+
+
+
