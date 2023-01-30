c061060edda55b794e8f762f1a7b51830f6bece3
EdgarShay <97201888+EdgarShay@users.noreply.github.com>
2023-01-30 00:28:20 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,49 +1,134 @@
-<?php include('./includes/header.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Switch page of my Portal Page</title>
+    <link href="css/styles.css" type="text/css"
+    rel="stylesheet">
+</head>
+<body class="Switch">
+<h1><a href="index.php">Edgar's Portal Page</a></h1>
+<h2>The navigation below represents our Big 
+Assignments. </h2>
 
-<div id="wrapper">
 
-<div id="hero">
-<img src="images/twelve.jpg" alt="Twelve is greater than 3">
+ <div id="wrapper">
+<nav>
+<ul>
+<li><a href="index.php">Switch</a></li>
+<li><a href="troubleshoot.php">Troubleshoot</a></li>
+<li><a href="calculator.php">Calculator</a></li>
+<li><a href="email.php">Email</a></li>
+<li><a href="database.php">Database</a></li>
+<li><a href="gallery.php">Gallery</a></li>
+</ul>
+</nav>
+<div id="row">
 
 
-<!--end hero-->
-</div> 
+</div>
 
 <main>
-<h1>Welcome to our Web App Programming Class!</h1>
-<h2>We are going to learn PHP!</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-sed do eiusmod tempor incididunt ut labore et dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud 
-exercitationullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit 
-in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-<h2>Another Headline 2!</h2>
-<p>Sed ut perspiciatis unde omnis iste natus error 
-sit voluptatem accusantium doloremque laudantium, totam rem
-aperiam, eaque ipsa quae ab illo inventore veritatis 
-et quasi architecto beatae vitae dicta sunt explicabo. 
-Nemo enim ipsam voluptatem quia voluptas sit aspernatur 
-aut odit aut fugit, sed quia consequuntur magni dolores 
-eos qui ratione voluptatem sequi nesciunt. Neque porro 
-quisquam est, qui dolorem ipsum quia dolor sit amet, 
-consectetur,</p>
+
+<h2>About Edgar</h2>
+<img class="right" src="images/edgar.jpg" alt="My-image">
+
+<p>Hello my name is Edgar Vargas. I moved to Seattle from Puerto Vallarta, Mexico 
+almost eight years ago. My first language is Spanish. This is my fifth Web Develepment class
+here at Seattle Central College. I have a wobnderful wife and a handsome boy.
+My goal is to earn my certificate in Web Development and eventually get my degree
+in Computer Science. By gaining experience and skills, I plan to grow my career. 
+Besides my education I have two real passions. The first is spending time with my wife and son.
+The other passion is to working out in the gym.  </p>
+<div class="container">
+<img class="left" src="images/screenshot.jpg" alt="MAMP">
+<img class="left" src="images/screenshot.2.jpg" alt="MAMP-display-errors">
+<img class="left" src="images/screenshot.3.jpg" alt="MAMP-local-host-error">
+</div>
 </main>
 
 <aside>
-<h3>This is our headline three in our beautiful aside</h3>
-<p>Ut enim ad minim veniam, quis nostrud 
-exercitationullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit 
-in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+<h2>Weekly Class Exercises</h2>
+<h3>Week 2</h3>
+<ol>
+<li><a href="weeks/week2/var.php">var.php</a></li>
+<li><a href="weeks/week2/var2.php">var2.php</a></li>
+<li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
+<li><a href="weeks/week2/currency.php">currency.php</a></li>
+<li><a href="weeks/week2/heredoc.php.">heredoc.php</a></li>
+</ol>
+
+<h3>Week 3</h3>
+<ol>
+<li><a href="weeks/week3/if.php">if.php</a></li>
+<li><a href="weeks/week3/date.php">date.php</a></li>
+<li><a href="weeks/week3/for-each.php">for-each.php</a></li>
+<li><a href="weeks/week3/index.php">index.php</a></li>
+<li><a href="weeks/week3/for-loop.php">for-loop.php</a></li>
+<li><a href="weeks/week3/about.php">about.php</a></li>
+<li><a href="weeks/week3/switch.php">switch.php</a></li>
+</ol>
+
+<h3>Week 4</h3>
+<ol>
+<li><a href="weeks/week4/adder.php">adder.php</a></li>
+<li><a href="weeks/week4/arithmetic.php">arithmetic.php</a></li>
+<li><a href="weeks/week4/celcius.php">celcius.php</a></li>
+<li><a href="weeks/week4/form1.php">form1.php</a></li>
+<li><a href="weeks/week4/form2.php">form2.php</a></li>
+<li><a href="weeks/week4/form3.php">form3.php</a></li>
+<li><a href="weeks/week4/form-get.php">form-get.php</a></li>
+</ol>
+
+<h3>Week 5</h3>
+<ol>
+<li><a href="weeks/week5/currency1.php">currency1.php</a></li>
+<li><a href="weeks/week5/currency2.php">currency2.php</a></li>
+<li><a href="weeks/week5/currency3.php">currency3.php</a></li>
+<li><a href="weeks/week5/currency4.php">currency4.php</a></li>
+<li><a href="weeks/week5/null.php">null.php</a></li>
+</ol>
+
+<h3>Week 6</h3>
+<ol>
+<li><a href="weeks/week6/form1.php">form1.php</a></li>
+<li><a href="weeks/week6/functions.php">functions.php</a></li>
+<li><a href="weeks/week6/implode.php">implode.php</a></li>
+</ol>
+
+<h3>Week 7</h3>
+<ol>
+<li><a href="weeks/week7/pictures.php">pictures.php</a></li>
+<li><a href="weeks/week7/random.php">ramdom.php</a></li>
+<li><a href="weeks/week7/strings.php">strings.php</a></li>
+</ol>
+
+<h3>Week 8</h3>
+<ol>
+<li><a href="weeks/week8/people.php">people.php</a></li>
+</ol>
 
 </aside>
 
-<!--end wrapper-->
-</div> 
+<footer>
+    <ul>
+        <li>Copyright &copy; 2023</li>
+        <li>All Rights Reserved</li>
+        <li><a href="">Terms of use</a></li>
+        <li><a href="../index.html">Web Design by Edgar</a></li>
+        <li><a href="">
+        <img src="images/html5.png" alt="HTML 5 logo">
+        </a></li>
+    </ul>
+    
+</footer>
 
-<?php
-include('./includes/footer.php');
 
+<!-- close wrapper -->
+</div>
 
-
+</body>
+</html>
