b04bc759ae4339420db0894e4de72930778d44ca
EdgarShay <97201888+EdgarShay@users.noreply.github.com>
2023-02-10 01:18:36 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -87,6 +87,8 @@ The other passion is to working out in the gym.  </p>
 <ol>
 <li><a href="weeks/week5/calculator.php">calculator.php</a></li>
 <li><a href="weeks/week5/calculator2.php">calculator2.php</a></li>
+<li><a href="weeks/week5/calculator2.php">calculator3.php</a></li>
+<li><a href="weeks/week5/calculator2.php">calculator4.php</a></li>
 <li><a href="weeks/week5/currency1.php">currency1.php</a></li>
 <li><a href="weeks/week5/currency2.php">currency2.php</a></li>
 <li><a href="weeks/week5/currency3.php">currency3.php</a></li>
